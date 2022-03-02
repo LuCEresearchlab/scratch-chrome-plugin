5cfa912f268269bfb315759f0022c055309cc7eb
Alen Sugimoto <alensugimoto@gmail.com>
2022-03-02 16:31:03 +0100

    Extract inject code

--- popup.js
+++ popup.js
@@ -18,21 +18,6 @@ changeColor.addEventListener("click", async () => {
 // The body of this function will be executed as a content script inside the
 // current page
 function setPageBackgroundColor() {
-	/**
-	 * injectScript - Inject internal script to available access to the `window`
-	 *
-	 * @param  {type} file_path Local path of the internal script.
-	 * @param  {type} tag The tag as string, where the script will be append (default: 'body').
-	 * @see    {@link http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
-	 */
-	function injectScript(file_path, tag) {
-		var node = document.getElementsByTagName(tag)[0];
-		var script = document.createElement("script");
-		script.setAttribute("type", "text/javascript");
-		script.setAttribute("src", file_path);
-		node.appendChild(script);
-	}
-	injectScript(chrome.runtime.getURL("content.js"), "body");
 	chrome.storage.sync.get("color", ({ color }) => {
 		document.body.style.backgroundColor = color;
 	});
