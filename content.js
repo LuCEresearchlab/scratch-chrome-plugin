392171ccf32015867699eb8d0281a09894d5968a
Alen Sugimoto <alensugimoto@gmail.com>
2022-03-05 08:20:45 +0100

    Fix most of lint errors

--- content.js
+++ content.js
@@ -1,88 +1,43 @@
-// document.addEventListener("click", messageContentScript);
+/* eslint-disable no-underscore-dangle */
 
 // const json = require('./scratch-expression-blocks.json');
-
-function messageContentScript() {
-  window.postMessage(
-    {
-      direction: 'from-page-script',
-      message: 'JSON.parse(JSON.stringify(Blockly.getMainWorkspace()))',
-    },
-    '*',
-  );
-}
-
-// Object.values(Blockly.getMainWorkspace().blockDB_).forEach(function (el) {
-// 	el.svgPath_.addEventListener("mousedown", () => {
-// 		window.postMessage(
-// 			{
-// 				direction: "from-page-script",
-// 				message:
-// 					"JSON.parse(JSON.stringify(Blockly.getMainWorkspace()))",
-// 			},
-// 			"*"
-// 		);
-// 	});
-// });
-const svgns = 'http://www.w3.org/2000/svg';
-
-// if (Blocky) {
-
-// }
-
 // eslint-disable-next-line no-undef
-
-const workspace = Blockly.getMainWorkspace();
-workspace.addChangeListener((event) => {
-  console.log(event.type);
-})
-workspace.addChangeListener((event) => {
-	if (event.type === 'endDrag') {
-		const block = workspace.getBlockById(event.blockId);
-    console.log(block.svgGroup_.querySelector('.exp'))
-
-    if (block.parentBlock_ === null) {
-      if (block.svgGroup_.querySelector('.exp') === null) {
-        const svgButton = document.createElementNS(svgns, "rect");
-        svgButton.classList.add("exp");
-        svgButton.setAttribute("data-block", event.blockId);
-        svgButton.setAttribute("x", "0");
-        svgButton.setAttribute("y", "-10");
-        svgButton.setAttribute("width", "100");
-        svgButton.setAttribute("height", "100");
-        svgButton.setAttribute("fill", "#5cceee");
-        // document.createElement("path");
-        // svgButton.className = "myName";
-    // svgButton.setAttribute('stroke', '#AA0000');
-    // svgButton.setAttribute('fill', "#AA0000");
-    // svgButton.setAttribute('d', `
-        //     M (CX - R), CY
-        //     a R,R 0 1,0 (R * 2),0
-        //     a R,R 0 1,0 -(R * 2),0
-        // `);
-  
-        svgButton.addEventListener('mousedown', (ev) => {
+if (Blockly) {
+  // eslint-disable-next-line no-undef
+  const workspace = Blockly.getMainWorkspace();
+  workspace.addChangeListener((event) => {
+    console.log(event.type);
+  });
+  workspace.addChangeListener((event) => {
+    if (event.type === 'endDrag') {
+      const block = workspace.getBlockById(event.blockId);
+      if (block.parentBlock_ === null) {
+        if (block.svgGroup_.querySelector('.exp') === null) {
+          const svgns = 'http://www.w3.org/2000/svg';
+          const svgButton = document.createElementNS(svgns, 'rect');
+          svgButton.classList.add('exp');
+          svgButton.setAttribute('data-block', event.blockId);
+          svgButton.setAttribute('x', '0');
+          svgButton.setAttribute('y', '-10');
+          svgButton.setAttribute('width', '100');
+          svgButton.setAttribute('height', '100');
+          svgButton.setAttribute('fill', '#5cceee');
+          svgButton.addEventListener('mousedown', (ev) => {
             const blockId = ev.target.dataset.block;
-            // const block = workspace.getBlockById(blockId);
-            console.log(blockId)
-        })
-        block.svgGroup_.append(svgButton);
-        // console.log(svgGroup_)
-      }
-    } else {
-      if (block.svgGroup_.querySelector('.exp') !== null) {
+            const b = workspace.getBlockById(blockId).toString();
+            window.postMessage(
+              {
+                direction: 'from-page-script',
+                message: b,
+              },
+              '*',
+            );
+          });
+          block.svgGroup_.append(svgButton);
+        }
+      } else if (block.svgGroup_.querySelector('.exp') !== null) {
         block.svgGroup_.removeChild(block.svgGroup_.querySelector('.exp'));
       }
-      }
-	}
-});
-
-window.addEventListener("message", function (event) {
-	if (
-		event.source == window &&
-		event.data &&
-		event.data.direction == "from-content-script"
-	) {
-		alert('Content script received message: "' + event.data.message + '"');
-	}
-});
+    }
+  });
+}
--- inject.js
+++ inject.js
@@ -1,33 +1,26 @@
-window.addEventListener("message", function (event) {
-	if (
-		event.source == window &&
-		event.data &&
-		event.data.direction == "from-page-script"
-	) {
-		alert('Content script received message: "' + event.data.message + '"');
-        console.log(event.data.message);
-        window.postMessage(
-            {
-                direction: "from-content-script",
-                message: "Blockly.getMainWorkspace()",
-            },
-            "*"
-        );
-	}
+window.addEventListener('message', (event) => {
+  if (
+    event.source === window
+    && event.data
+    && event.data.direction === 'from-page-script'
+  ) {
+    alert('Content script received message: "' + event.data.message + '"');
+  }
 });
 
 /**
  * injectScript - Inject internal script to available access to the `window`
  *
- * @param  {type} file_path Local path of the internal script.
+ * @param  {type} filePath Local path of the internal script.
  * @param  {type} tag The tag as string, where the script will be append (default: 'body').
  * @see    {@link http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
  */
-function injectScript(file_path, tag) {
-	var node = document.getElementsByTagName(tag)[0];
-	var script = document.createElement("script");
-	script.setAttribute("type", "text/javascript");
-	script.setAttribute("src", file_path);
-	node.appendChild(script);
+function injectScript(filePath, tag) {
+  const node = document.getElementsByTagName(tag)[0];
+  const script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', filePath);
+  node.appendChild(script);
 }
-injectScript(chrome.runtime.getURL("content.js"), "body");
+// eslint-disable-next-line no-undef
+injectScript(chrome.runtime.getURL('content.js'), 'body');
