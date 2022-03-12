d0cfdcc9e64a5aadf120b5b4089ce0ff72090c6d
Alen Sugimoto <alensugimoto@gmail.com>
2022-03-12 12:28:46 +0100

    Inject script only after page load

--- src/js/inject.js
+++ src/js/inject.js
@@ -1,3 +1,22 @@
+/**
+ * injectScript - Inject internal script to available access to the `window`
+ *
+ * @param  {type} filePath Local path of the internal script.
+ * @param  {type} tag The tag as string, where the script will be append (default: 'body').
+ * @see    {@link http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
+ */
+function injectScript(filePath, tag) {
+  const node = document.getElementsByTagName(tag)[0];
+  const script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', filePath);
+  node.appendChild(script);
+}
+
+function main() {
+  injectScript(chrome.runtime.getURL('content.js'), 'body');
+}
+
 window.addEventListener('message', (event) => {
   if (
     event.source === window
@@ -19,19 +38,4 @@ window.addEventListener('message', (event) => {
   }
 });
 
-/**
- * injectScript - Inject internal script to available access to the `window`
- *
- * @param  {type} filePath Local path of the internal script.
- * @param  {type} tag The tag as string, where the script will be append (default: 'body').
- * @see    {@link http://stackoverflow.com/questions/20499994/access-window-variable-from-content-script}
- */
-function injectScript(filePath, tag) {
-  const node = document.getElementsByTagName(tag)[0];
-  const script = document.createElement('script');
-  script.setAttribute('type', 'text/javascript');
-  script.setAttribute('src', filePath);
-  node.appendChild(script);
-}
-// eslint-disable-next-line no-undef
-injectScript(chrome.runtime.getURL('content.js'), 'body');
+window.addEventListener('load', main);
