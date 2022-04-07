LuCEresearchlab/scratch-chrome-plugin

commit c5e0df3c8d3724dec94643b1adfaa4c17ef01738

Refactor getCachedVmValue

src/content/utils/scratchVmUtils.js
@@ -20,20 +20,17 @@ export const typeToDefaultValue = (type) => {
 export const getCachedVmValue = (block, type, thread) => {
   const { runtime } = getScratchVM();
   const c = runtime._editingTarget.blocks._cache._executeCached[block.id];
-  if (c._isShadowBlock) {
-    if (!c._shadowValue) {
-      return typeToDefaultValue(type);
-    }
-    if (type === 'String') {
-      return `"${c._shadowValue}"`;
-    }
-    return String(c._shadowValue);
-  }
+  let val;
   if (c._parentKey) {
-    return String(c._parentValues[c._parentKey]);
+    val = String(c._parentValues[c._parentKey]);
+  } else {
+    if (thread.topBlock !== block.id) {
+      throw new Error('Invalid block being evaluated');
+    }
+    val = String(thread.justReported);
   }
-  if (thread.topBlock !== block.id) {
-    throw new Error('Invalid block being evaluated');
+  if (type === 'String') {
+    val = `"${val}"`;
   }
-  return String(thread.justReported);
+  return val;
 };

commit 97ad0247dd4e5d5c55c4d7dbddd4eabdeb9f9889

Fix bug introduced by c5e0df3c8d3724dec94643b1adfaa4c17ef01738

src/content/utils/scratchVmUtils.js
@@ -9,27 +9,41 @@ export const connectionToType = (con) => {
   return con.check_[0];
 };
 
-export const typeToDefaultValue = (type) => {
-  if (type === 'Boolean') {
-    return 'false';
+const typeToDefaultRawValue = (type) => {
+  switch (type) {
+    case 'Boolean': return 'false';
+    case 'Number': return '0';
+    case 'String': return '';
+    default: throw new Error('Unknown type');
   }
-  throw new Error(`Unknown default value for type ${type}`);
+};
+
+const unrawValue = (type, value) => (type === 'String' ? `"${value}"` : value);
+
+export const typeToDefaultValue = (type) => {
+  const raw = typeToDefaultRawValue(type);
+  return unrawValue(type, raw);
 };
 
 export const getCachedVmValue = (block, type, thread) => {
-  const { runtime } = getScratchVM();
-  const c = runtime._editingTarget.blocks._cache._executeCached[block.id];
-  let val;
-  if (c._parentKey) {
-    val = String(c._parentValues[c._parentKey]);
-  } else {
+  const getRawValue = () => {
+    const { runtime } = getScratchVM();
+    const c = runtime._editingTarget.blocks._cache._executeCached[block.id];
+    if (c._isShadowBlock) {
+      if (!c._shadowValue) {
+        return typeToDefaultRawValue(type);
+      }
+      return String(c._shadowValue);
+    }
+    if (c._parentKey) {
+      return String(c._parentValues[c._parentKey]);
+    }
     if (thread.topBlock !== block.id) {
       throw new Error('Invalid block being evaluated');
     }
-    val = String(thread.justReported);
-  }
-  if (type === 'String') {
-    val = `"${val}"`;
-  }
-  return val;
+    return String(thread.justReported);
+  };
+
+  const raw = getRawValue();
+  return unrawValue(type, raw);
 };