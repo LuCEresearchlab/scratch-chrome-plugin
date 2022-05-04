8ed327d284b6c6db4de15a3d4a2c01ce1e0cdd22
Alen Sugimoto <alensugimoto@gmail.com>
2022-05-04 14:35:48 +0200

    Get possible opcodes of nodes in tree

--- src/content/components/Tree/Tree.js
+++ src/content/components/Tree/Tree.js
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import { ExpressionTreeEditor } from 'react-expression-tree';
+
+import { tutorToService } from '../../utils/serviceToTutor.js';
+import typeToMsg, {
+  argumentPlaceholder,
+  connectorPlaceholder,
+  listPlaceholder,
+  shadowPlaceholder,
+  variablePlaceholder,
+} from '../../../assets/data/scratch_type_to_msg.js';
+import { getBlockly } from '../../utils/stateHandler.js';
+
+function nodeToOpcode(node) {
+  let str = '';
+  node.content.forEach((part) => {
+    if (part.type === 'hole') {
+      str += connectorPlaceholder;
+    } else {
+      str += part.content;
+    }
+  });
+  const opcodes = [];
+  Object.entries(typeToMsg).forEach((entry) => {
+    const key = entry[0];
+    const value = entry[1];
+    let msgs;
+    if (!Array.isArray(value)) {
+      msgs = [value];
+    } else {
+      msgs = value;
+    }
+    msgs = msgs.flatMap((msg) => {
+      // assume msg contains at most one placeholder
+      if (msg.includes(variablePlaceholder)) {
+        const variableList = getBlockly().mainWorkspace.getVariablesOfType('');
+        return variableList.map((v) => msg.replace(variablePlaceholder, v.name));
+      } if (msg.includes(listPlaceholder)) {
+        const listList = getBlockly().mainWorkspace.getVariablesOfType('list');
+        return listList.map((l) => msg.replace(listPlaceholder, l.name));
+      }
+      return msg;
+    });
+    if (msgs.includes(shadowPlaceholder)
+        || msgs.includes(argumentPlaceholder)
+        || msgs.includes(str)) {
+      opcodes.push(key);
+    }
+  });
+  return opcodes;
+}
+
+function getChildNodes(node, diagram) {
+  const nodeId = node.nodePlug.valA;
+  const outEdges = diagram.edges.filter((edge) => edge.plugA.valA === nodeId);
+  const childIds = outEdges.map((edge) => edge.plugB.valA);
+  return diagram.nodes.filter((child) => childIds.includes(child.nodePlug.valA));
+}
+
+function labelDiagram(diagram) {
+  const labelNode = (node) => {
+    node.type = nodeToOpcode(node);
+    getChildNodes(node, diagram).forEach((child) => {
+      labelNode(child);
+    });
+  };
+  labelNode(diagram.root);
+}
 
 function Tree({ autolayout, diagram, setTemporaryDiagram }) {
   const {
@@ -12,6 +78,13 @@ function Tree({ autolayout, diagram, setTemporaryDiagram }) {
     edges,
     selectedRootNode,
   } = diagram;
+
+  const setTempDiagram = useCallback((state, payload) => {
+    const diagram = tutorToService(state);
+    labelDiagram(diagram);
+    console.log(diagram);
+    setTemporaryDiagram(state, payload);
+  }, [setTemporaryDiagram]);
 
   return (
     <>
@@ -24,7 +97,7 @@ function Tree({ autolayout, diagram, setTemporaryDiagram }) {
         nodes={nodes}
         edges={edges}
         selectedRootNode={selectedRootNode}
-        onStateChange={setTemporaryDiagram}
+        onStateChange={setTempDiagram}
       />
     </>
   );
--- test/diagramUtils.spec.js
+++ test/diagramUtils.spec.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-underscore-dangle */
 import assert from 'assert';
 import createDiagramTests from './fixtures/createDiagramTests.js';
 import createDiagram from '../src/content/utils/diagramUtils.js';
