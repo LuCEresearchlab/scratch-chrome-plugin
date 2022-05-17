465f86a8a4c539b87a372a7eff58de724e3c5a43
Alen Sugimoto <alensugimoto@gmail.com>
2022-05-17 11:12:33 +0200

    Fix DOMParser not defined

--- test/tutorToBlock.spec.js
+++ test/tutorToBlock.spec.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import ScratchVM from 'scratch-vm';
+import jsdomGlobal from 'jsdom-global';
 
 import { labelDiagramWithOpcodes } from '../src/content/utils/tutorToBlock.js';
 import labelDiagramWithOpcodesTests from './fixtures/tutorToBlock/labelDiagramWithOpcodesTests.js';
@@ -32,30 +33,30 @@ const blockly = {
     enable: () => {},
   },
   DataCategory: () => [
-    '<button xmlns="http://www.w3.org/1999/xhtml" text=…Variable" callbackkey="CREATE_VARIABLE"></button>',
-    '<block type="data_variable" gap="8" id="h%{K[ti:.2…letype="\'\'">123</field>undefinedundefined</block>',
-    '<block type="data_variable" gap="24" id="`jEk@4|i[…\'\'">my variable</field>undefinedundefined</block>',
-    '<block type="data_setvariableto" gap="8"><field na…TEXT">0</field></shadow></value>undefined</block>',
-    '<block type="data_changevariableby" gap="8"><field…"NUM">1</field></shadow></value>undefined</block>',
-    '<block type="data_showvariable" gap="8"><field nam…letype="\'\'">123</field>undefinedundefined</block>',
-    '<block type="data_hidevariable" gap="8"><field nam…letype="\'\'">123</field>undefinedundefined</block>',
-    '<button xmlns="http://www.w3.org/1999/xhtml" text="Make a List" callbackkey="CREATE_LIST"></button>',
-    '<block type="data_listcontents" gap="24" id=")`0_y…type="list">sdf</field>undefinedundefined</block>',
-    '<block type="data_addtolist" gap="8"><field name="…">thing</field></shadow></value>undefined</block>',
+    '<button text="Make a Variable" callbackkey="CREATE_VARIABLE"></button>',
+    "<block type=\"data_variable\" gap=\"8\" id=\"h%{K[ti:.25q6W(B51Ry\"><field name=\"VARIABLE\" id=\"h%{K[ti:.25q6W(B51Ry\" variabletype=\"''\">123</field>undefinedundefined</block>",
+    "<block type=\"data_variable\" gap=\"24\" id=\"`jEk@4|i[#Fk?(8x)AV.-my variable\"><field name=\"VARIABLE\" id=\"`jEk@4|i[#Fk?(8x)AV.-my variable\" variabletype=\"''\">my variable</field>undefinedundefined</block>",
+    "<block type=\"data_setvariableto\" gap=\"8\"><field name=\"VARIABLE\" id=\"h%{K[ti:.25q6W(B51Ry\" variabletype=\"''\">123</field><value name=\"VALUE\"><shadow type=\"text\"><field name=\"TEXT\">0</field></shadow></value>undefined</block>",
+    "<block type=\"data_changevariableby\" gap=\"8\"><field name=\"VARIABLE\" id=\"h%{K[ti:.25q6W(B51Ry\" variabletype=\"''\">123</field><value name=\"VALUE\"><shadow type=\"math_number\"><field name=\"NUM\">1</field></shadow></value>undefined</block>",
+    "<block type=\"data_showvariable\" gap=\"8\"><field name=\"VARIABLE\" id=\"h%{K[ti:.25q6W(B51Ry\" variabletype=\"''\">123</field>undefinedundefined</block>",
+    "<block type=\"data_hidevariable\" gap=\"8\"><field name=\"VARIABLE\" id=\"h%{K[ti:.25q6W(B51Ry\" variabletype=\"''\">123</field>undefinedundefined</block>",
+    '<button text="Make a List" callbackkey="CREATE_LIST"></button>',
+    '<block type="data_listcontents" gap="24" id=")`0_yuooimQ5)^|1?hog"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field>undefinedundefined</block>',
+    '<block type="data_addtolist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field><value name="ITEM"><shadow type="text"><field name="TEXT">thing</field></shadow></value>undefined</block>',
     '<sep gap="36"/>',
-    '<block type="data_deleteoflist" gap="8"><field nam…"NUM">1</field></shadow></value>undefined</block>',
-    '<block type="data_deletealloflist" gap="8"><field …type="list">sdf</field>undefinedundefined</block>',
-    '<block type="data_insertatlist" gap="8"><field nam…ame="TEXT">thing</field></shadow></value></block>',
-    '<block type="data_replaceitemoflist" gap="8"><fiel…ame="TEXT">thing</field></shadow></value></block>',
+    '<block type="data_deleteoflist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field><value name="INDEX"><shadow type="math_integer"><field name="NUM">1</field></shadow></value>undefined</block>',
+    '<block type="data_deletealloflist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field>undefinedundefined</block>',
+    '<block type="data_insertatlist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field><value name="INDEX"><shadow type="math_integer"><field name="NUM">1</field></shadow></value><value name="ITEM"><shadow type="text"><field name="TEXT">thing</field></shadow></value></block>',
+    '<block type="data_replaceitemoflist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field><value name="INDEX"><shadow type="math_integer"><field name="NUM">1</field></shadow></value><value name="ITEM"><shadow type="text"><field name="TEXT">thing</field></shadow></value></block>',
     '<sep gap="36"/>',
-    '<block type="data_itemoflist" gap="8"><field name=…"NUM">1</field></shadow></value>undefined</block>',
-    '<block type="data_itemnumoflist" gap="8"><field na…">thing</field></shadow></value>undefined</block>',
-    '<block type="data_lengthoflist" gap="8"><field nam…type="list">sdf</field>undefinedundefined</block>',
-    '<block type="data_listcontainsitem" gap="8"><field…">thing</field></shadow></value>undefined</block>',
+    '<block type="data_itemoflist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field><value name="INDEX"><shadow type="math_integer"><field name="NUM">1</field></shadow></value>undefined</block>',
+    '<block type="data_itemnumoflist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field><value name="ITEM"><shadow type="text"><field name="TEXT">thing</field></shadow></value>undefined</block>',
+    '<block type="data_lengthoflist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field>undefinedundefined</block>',
+    '<block type="data_listcontainsitem" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field><value name="ITEM"><shadow type="text"><field name="TEXT">thing</field></shadow></value>undefined</block>',
     '<sep gap="36"/>',
-    '<block type="data_showlist" gap="8"><field name="L…type="list">sdf</field>undefinedundefined</block>',
-    '<block type="data_hidelist" gap="8"><field name="L…type="list">sdf</field>undefinedundefined</block>',
-  ],
+    '<block type="data_showlist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field>undefinedundefined</block>',
+    '<block type="data_hidelist" gap="8"><field name="LIST" id=")`0_yuooimQ5)^|1?hog" variabletype="list">sdf</field>undefinedundefined</block>',
+  ].map(blockly.Xml.textToDom),
   mainWorkspace: {
     getVariablesOfType: (type = '') => {
       switch (type) {
@@ -85,6 +86,11 @@ const scratchTB = {
 
 describe('src/content/utils/tutorToBlock', () => {
   describe('labelDiagramWithOpcodes(diagram)', () => {
+    it('setup', () => {
+      jsdomGlobal();
+      global.DOMParser = window.DOMParser;
+    });
+
     labelDiagramWithOpcodesTests.forEach((test) => {
       it(test.it, () => {
         assert.deepEqual(labelDiagramWithOpcodes(...test.args, blockly, scratchTB), test.expected);
